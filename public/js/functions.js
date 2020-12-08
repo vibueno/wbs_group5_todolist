2f9a6f2d92e37d59e4556f514a0ba2e5770c11d8
memo40 <memocann4040@gmail.com>
2020-12-08 16:33:16 +0100

    html sections

---
+++ public/js/functions.js
@@ -0,0 +1,41 @@
+let button = document.getElementById("button");
+let container = document.getElementById("container");
+let input  = document.getElementById("input");
+
+
+button.addEventListener("click",()=>{
+    var paragraph = document.createElement("p");
+    paragraph.innerText=input.value;
+    container.appendChild(paragraph);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    

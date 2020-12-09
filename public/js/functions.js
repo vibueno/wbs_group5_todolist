43f40a876728f451311e909171ff66fc25c41437
DavidSurina <david.surina22@gmail.com>
2020-12-09 12:06:49 +0100

    corrected small things in js

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,8 +3,8 @@ const container = document.getElementById("tasks");
 const input  = document.getElementById("user_input");
 
 
- function AddTask() {
-    const Parent = document.querySelector(".section-tasks__todo-wrapper");
+ function addTask() {
+    const parent = document.querySelector(".section-tasks__todo-wrapper");
     const input = document.getElementById("user_input").value;
     if(input) {
     let  newTask = `<div class="section-tasks__task">
@@ -12,13 +12,13 @@ const input  = document.getElementById("user_input");
           <button class="section-tasks__task-button"><i class="fas fa-check"></i></button>
           <button class="section-tasks__task-button"><i class="fas fa-trash-alt"></i></button>
         </div>`;
-    Parent.innerHTML+=newTask;
+    parent.innerHTML+=newTask;
     } else {
         alert("Please add some task");
     }
 }
 
-button.addEventListener("click",AddTask);
+button.addEventListener("click",addTask);
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 document.getElementById("add_task").addEventListener("click", function(e){
